8dfa77e1bd5f8cba0497f463ca55bb373faf29ea
MuditJain25 <muditjain0081@gmail.com>
2025-10-01 16:42:11 +0530

    Added Navlink in  AdminNavbar.jsx

--- admin-panel/pages/AdminNavbar.jsx
+++ admin-panel/pages/AdminNavbar.jsx
@@ -1,17 +1,17 @@
 import React from "react";
-import { useState } from "react";
+import { NavLink } from "react-router-dom";
 
-export default function AdminNavbar({ setPage }) {
+export default function AdminNavbar() {
   return (
     <nav className="bg-blue-600 text-white p-4 flex justify-between">
       <h1 className="font-bold text-lg">Admin Panel</h1>
       <div className="space-x-4">
-        <button onClick={() => setPage("tickets")} className="hover:underline">
+        <NavLink className="hover:underline" to={`/admin/tickets`}            >
           Tickets
-        </button>
-        <button onClick={() => setPage("addActivity")} className="hover:underline">
-          Add Activity
-        </button>
+         </NavLink>
+         <NavLink className="hover:underline" to={`/admin/activity`}            >
+          Activities
+         </NavLink>
       </div>
     </nav>
   );
